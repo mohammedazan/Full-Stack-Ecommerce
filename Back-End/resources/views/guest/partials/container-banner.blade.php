9f8b891e5717d8ea22c979a036ef66a1f246cd3a
mohammedazan <mohammedazin0@gmail.com>
2024-06-04 22:03:06 +0100

    Add container-banner content, images and text

--- Back-End/resources/views/guest/partials/container-banner.blade.php
+++ Back-End/resources/views/guest/partials/container-banner.blade.php
@@ -2,45 +2,66 @@
       <div class="container">
                 <div class="row">
                     <div class="col-sm-6 col-lg-3">
+                        @php
+                        $desiredKey = 2; // Change this value to the desired key (0 for the first feature, 1 for the second, etc.)
+                        @endphp
+                        @foreach($featuredImage as $key => $feature)
+                        @if($key == $desiredKey)
                         <div class="banner banner-overlay">
                             <a href="#">
-                                <img src="assets/images/demos/demo-13/banners/banner-1.jpg" alt="Banner">
+                                <img src="{{ asset($feature->image) }}" alt="Banner">
                             </a>
-
                             <div class="banner-content">
-                                <h4 class="banner-subtitle text-white"><a href="#">Weekend Sale</a></h4><!-- End .banner-subtitle -->
-                                <h3 class="banner-title text-white"><a href="#">Lighting <br>& Accessories <br><span>25% off</span></a></h3><!-- End .banner-title -->
+                                <h3 class="banner-title text-white"><a href="#">{{ $feature->title }}</a></h3><!-- End .banner-title -->
+                                <h4 class="banner-subtitle text-white"><a href="#">{{ $feature->link }}</a></h4><!-- End .banner-subtitle -->
                                 <a href="#" class="banner-link">Shop Now <i class="icon-long-arrow-right"></i></a>
                             </div><!-- End .banner-content -->
                         </div><!-- End .banner -->
+                        @break
+                        @endif
+                        @endforeach
                     </div><!-- End .col-lg-3 -->
 
                     <div class="col-sm-6 col-lg-3 order-lg-last">
+                        @php
+                        $desiredKey = 1; // Change this value to the desired key (0 for the first feature, 1 for the second, etc.)
+                        @endphp
+                        @foreach($featuredImage as $key => $feature)
+                        @if($key == $desiredKey)
                         <div class="banner banner-overlay">
                             <a href="#">
-                                <img src="assets/images/demos/demo-13/banners/banner-3.jpg" alt="Banner">
+                                <img src="{{ asset($feature->image) }}" alt="Banner" >
                             </a>
-
                             <div class="banner-content">
-                                <h4 class="banner-subtitle text-white"><a href="#">Smart Offer</a></h4><!-- End .banner-subtitle -->
-                                <h3 class="banner-title text-white"><a href="#">Anniversary <br>Special <br><span>15% off</span></a></h3><!-- End .banner-title -->
+                                <h3 class="banner-title text-white"><a href="#">{{ $feature->title }}</a></h3><!-- End .banner-title -->
+                                <h4 class="banner-subtitle text-white">{{ $feature->link }}</h4><!-- End .banner-subtitle -->
                                 <a href="#" class="banner-link">Shop Now <i class="icon-long-arrow-right"></i></a>
                             </div><!-- End .banner-content -->
                         </div><!-- End .banner -->
+                        @break
+                        @endif
+                    @endforeach
                     </div><!-- End .col-lg-3 -->
-
-                    <div class="col-lg-6">
-                        <div class="banner banner-overlay">
-                            <a href="#">
-                                <img src="assets/images/demos/demo-13/banners/banner-2.jpg" alt="Banner">
-                            </a>
-
-                            <div class="banner-content">
-                                <h4 class="banner-subtitle text-white d-none d-sm-block"><a href="#">Amazing Value</a></h4><!-- End .banner-subtitle -->
-                                <h3 class="banner-title text-white"><a href="#">Clothes Trending <br>Spring Collection 2019 <br><span>from $12,99</span></a></h3><!-- End .banner-title -->
-                                <a href="#" class="banner-link">Discover Now <i class="icon-long-arrow-right"></i></a>
-                            </div><!-- End .banner-content -->
-                        </div><!-- End .banner -->
+                    
+                    <div class="col-lg-6 ">
+                        @php
+                            $desiredKey = 0; // Change this value to the desired key (0 for the first feature, 1 for the second, etc.)
+                        @endphp
+                        @foreach($featuredImage as $key => $feature)
+                            @if($key == $desiredKey)
+                                <div class="banner banner-overlay">
+                                    <a href="#">
+                                        <img src="{{ asset($feature->image) }}" alt="Banner">
+                                    </a>
+                                    <div class="banner-content">
+                                        <h3 class="banner-title text-white"><a href="#">{{ $feature->title }}</a></h3><!-- End .banner-title -->
+                                        <h4 class="banner-subtitle text-white d-none d-sm-block">{{ $feature->link }}</h4><!-- End .banner-subtitle -->
+                                        <a href="#" class="banner-link">Discover Now <i class="icon-long-arrow-right"></i></a>
+                                    </div><!-- End .banner-content -->
+                                </div><!-- End .banner -->
+                                @break
+                            @endif
+                        @endforeach
                     </div><!-- End .col-lg-6 -->
                 </div><!-- End .row -->
             </div>
