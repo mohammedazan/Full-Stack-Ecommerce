6d473bf198bdb8f0abecbb1a43680b8979d9587a
hashem <hashem@.com>
2024-06-04 22:41:00 +0100

    add pages all

--- Back-End/app/Http/Controllers/GuestController.php
+++ Back-End/app/Http/Controllers/GuestController.php
@@ -14,4 +14,49 @@ class GuestController extends Controller
         $offer = Offer::where('deleted', 0)->get();
         return view('guest/home')->with(compact('productSubcategory','category','offer'));
     }
+
+
+    public function about(){
+        $productSubcategory = ProductSubCategory::where('deleted', 0)->where('status', 1)->get();
+        $category = ProductCategory::where('status', 1)->where('deleted', 0)->get();
+        return view('guest/pages.about')->with(compact('productSubcategory','category'));
+
+    }
+
+    public function contact(){
+        $productSubcategory = ProductSubCategory::where('deleted', 0)->where('status', 1)->get();
+        $category = ProductCategory::where('status', 1)->where('deleted', 0)->get();
+        return view('guest/pages.contact')->with(compact('productSubcategory','category'));
+    }
+
+    public function checkout(){
+        $productSubcategory = ProductSubCategory::where('deleted', 0)->where('status', 1)->get();
+        $category = ProductCategory::where('status', 1)->where('deleted', 0)->get();
+        return view('guest/pages.checkout')->with(compact('productSubcategory','category'));
+    }
+
+    public function wishlist(){
+        $productSubcategory = ProductSubCategory::where('deleted', 0)->where('status', 1)->get();
+        $category = ProductCategory::where('status', 1)->where('deleted', 0)->get();
+        return view('guest/pages.wishlist')->with(compact('productSubcategory','category'));
+    }
+
+    public function cart(){
+        $productSubcategory = ProductSubCategory::where('deleted', 0)->where('status', 1)->get();
+        $category = ProductCategory::where('status', 1)->where('deleted', 0)->get();
+        return view('guest/pages.cart')->with(compact('productSubcategory','category'));
+    }
+
+    public function product(){
+        $productSubcategory = ProductSubCategory::where('deleted', 0)->where('status', 1)->get();
+        $category = ProductCategory::where('status', 1)->where('deleted', 0)->get();
+        return view('guest/pages.product')->with(compact('productSubcategory','category'));
+    }
+
+    public function productdetail(){
+        $productSubcategory = ProductSubCategory::where('deleted', 0)->where('status', 1)->get();
+        $category = ProductCategory::where('status', 1)->where('deleted', 0)->get();
+        return view('guest/pages.productdetail')->with(compact('productSubcategory','category'));
+    }
+    
 }
---
+++ Back-End/resources/views/guest/pages/contact.blade.php
@@ -0,0 +1,294 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- molla/index-13.html  22 Nov 2019 09:59:06 GMT -->
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> about</title>
+    <meta name="keywords" content="HTML5 Template">
+    <meta name="description" content="Molla - Bootstrap eCommerce Template">
+    <meta name="author" content="p-themes">
+    <!-- Favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/icons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/icons/favicon-16x16.png">
+    <link rel="manifest" href="assets/images/icons/site.html">
+    <link rel="mask-icon" href="assets/images/icons/safari-pinned-tab.svg" color="#666666">
+    <link rel="shortcut icon" href="assets/images/icons/favicon.ico">
+    <meta name="apple-mobile-web-app-title" content="Molla">
+    <meta name="application-name" content="Molla">
+    <meta name="msapplication-TileColor" content="#cc9966">
+    <meta name="msapplication-config" content="assets/images/icons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <link rel="stylesheet" href="assets/vendor/line-awesome/line-awesome/line-awesome/css/line-awesome.min.css">
+    <!-- Plugins CSS File -->
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/plugins/owl-carousel/owl.carousel.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/plugins/magnific-popup/magnific-popup.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/plugins/jquery.countdown.css')}}" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/skins/skin-demo-13.css')}}" rel="stylesheet">
+    <link href="{{asset('assets/css/demos/demo-13.css')}}" rel="stylesheet">
+</head>
+
+<body>
+    <div class="page-wrapper">
+    
+    @include('guest/partials.header')
+        <!-- End .header -->
+ 
+        
+        <main class="main">
+    <div class="page-header text-center" style="background-image: url('assets/images/page-header-bg.jpg')">
+        <div class="container">
+            <h1 class="page-title">Contact us <span>Pages</span></h1>
+        </div><!-- End .container -->
+    </div>
+    <!-- End .page-header -->
+    <nav aria-label="breadcrumb" class="breadcrumb-nav border-0 mb-0">
+        <div class="container">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Contact us </li>
+            </ol>
+        </div><!-- End .container -->
+    </nav><!-- End .breadcrumb-nav -->
+
+    <div class="page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div id="map" class="mb-5"></div><!-- End #map -->
+                </div><!-- End .col-md-8 -->
+                <div class="col-md-4">
+                    <div class="contact-box text-center">
+                        <h3>Office</h3>
+                        <address>1 New York Plaza, New York, <br>NY 10004, USA</address>
+                    </div><!-- End .contact-box -->
+                    <div class="contact-box text-center">
+                        <h3>Start a Conversation</h3>
+                        <div><a href="mailto:#">info@Molla.com</a></div>
+                        <div><a href="tel:#">+1 987-876-6543</a>, <a href="tel:#">+1 987-976-1234</a></div>
+                    </div><!-- End .contact-box -->
+                    <div class="contact-box text-center">
+                        <h3>Social</h3>
+                        <div class="social-icons social-icons-color justify-content-center">
+                            <a href="#" class="social-icon social-facebook" title="Facebook" target="_blank"><i class="icon-facebook-f"></i></a>
+                            <a href="#" class="social-icon social-twitter" title="Twitter" target="_blank"><i class="icon-twitter"></i></a>
+                            <a href="#" class="social-icon social-instagram" title="Instagram" target="_blank"><i class="icon-instagram"></i></a>
+                            <a href="#" class="social-icon social-youtube" title="Youtube" target="_blank"><i class="icon-youtube"></i></a>
+                            <a href="#" class="social-icon social-pinterest" title="Pinterest" target="_blank"><i class="icon-pinterest"></i></a>
+                        </div><!-- End .soial-icons -->
+                    </div><!-- End .contact-box -->
+                </div><!-- End .col-md-4 -->
+            </div><!-- End .row -->
+
+            <hr class="mt-3 mb-5 mt-md-1">
+            <div class="touch-container row justify-content-center">
+                <div class="col-md-9 col-lg-7">
+                    <div class="text-center">
+                        <h2 class="title mb-1">Get In Touch</h2><!-- End .title mb-2 -->
+                        <p class="lead text-primary">
+                            We collaborate with ambitious brands and people; we’d love to build something great together.
+                        </p><!-- End .lead text-primary -->
+                        <p class="mb-3">Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna.</p>
+                    </div><!-- End .text-center -->
+
+                    <form action="#" class="contact-form mb-2">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <label for="cname" class="sr-only">Name</label>
+                                <input type="text" class="form-control" id="cname" placeholder="Name *" required>
+                            </div><!-- End .col-sm-4 -->
+
+                            <div class="col-sm-4">
+                                <label for="cemail" class="sr-only">Name</label>
+                                <input type="email" class="form-control" id="cemail" placeholder="Email *" required>
+                            </div><!-- End .col-sm-4 -->
+
+                            <div class="col-sm-4">
+                                <label for="cphone" class="sr-only">Phone</label>
+                                <input type="tel" class="form-control" id="cphone" placeholder="Phone">
+                            </div><!-- End .col-sm-4 -->
+                        </div><!-- End .row -->
+
+                        <label for="csubject" class="sr-only">Subject</label>
+                        <input type="text" class="form-control" id="csubject" placeholder="Subject">
+
+                        <label for="cmessage" class="sr-only">Message</label>
+                        <textarea class="form-control" cols="30" rows="4" id="cmessage" required placeholder="Message *"></textarea>
+
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-outline-primary-2 btn-minwidth-sm">
+                                <span>SUBMIT</span>
+                                <i class="icon-long-arrow-right"></i>
+                            </button>
+                        </div><!-- End .text-center -->
+                    </form><!-- End .contact-form -->
+                </div><!-- End .col-md-9 col-lg-7 -->
+            </div><!-- End .row -->
+        </div><!-- End .container -->
+    </div><!-- End .page-content -->
+</main><!-- End .main -->
+
+        <!-- start .footer -->
+        @include('guest/partials.footer')
+        <!-- End .footer -->
+    </div>
+    <!-- End .page-wrapper -->
+    <button id="scroll-top" title="Back to Top"><i class="icon-arrow-up"></i></button>
+
+    <!-- End .mobile-menu-container -->
+
+    <!-- Sign in / Register Modal -->
+    <div class="modal fade" id="signin-modal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-body">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true"><i class="icon-close"></i></span>
+                    </button>
+
+                    <div class="form-box">
+                        <div class="form-tab">
+                            <ul class="nav nav-pills nav-fill nav-border-anim" role="tablist">
+                                <li class="nav-item">
+                                    <a class="nav-link active" id="signin-tab" data-toggle="tab" href="#signin" role="tab" aria-controls="signin" aria-selected="true">Sign In</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" id="register-tab" data-toggle="tab" href="#register" role="tab" aria-controls="register" aria-selected="false">Register</a>
+                                </li>
+                            </ul>
+                            <div class="tab-content" id="tab-content-5">
+                                <div class="tab-pane fade show active" id="signin" role="tabpanel" aria-labelledby="signin-tab">
+                                    <form action="#">
+                                        <div class="form-group">
+                                            <label for="singin-email">Username or email address *</label>
+                                            <input type="text" class="form-control" id="singin-email" name="singin-email" required>
+                                        </div><!-- End .form-group -->
+
+                                        <div class="form-group">
+                                            <label for="singin-password">Password *</label>
+                                            <input type="password" class="form-control" id="singin-password" name="singin-password" required>
+                                        </div><!-- End .form-group -->
+
+                                        <div class="form-footer">
+                                            <button type="submit" class="btn btn-outline-primary-2">
+                                                <span>LOG IN</span>
+                                                <i class="icon-long-arrow-right"></i>
+                                            </button>
+
+                                            <div class="custom-control custom-checkbox">
+                                                <input type="checkbox" class="custom-control-input" id="signin-remember">
+                                                <label class="custom-control-label" for="signin-remember">Remember Me</label>
+                                            </div><!-- End .custom-checkbox -->
+
+                                            <a href="#" class="forgot-link">Forgot Your Password?</a>
+                                        </div><!-- End .form-footer -->
+                                    </form>
+                                    <div class="form-choice">
+                                        <p class="text-center">or sign in with</p>
+                                        <div class="row">
+                                            <div class="col-sm-6">
+                                                <a href="#" class="btn btn-login btn-g">
+                                                    <i class="icon-google"></i>
+                                                    Login With Google
+                                                </a>
+                                            </div><!-- End .col-6 -->
+                                            <div class="col-sm-6">
+                                                <a href="#" class="btn btn-login btn-f">
+                                                    <i class="icon-facebook-f"></i>
+                                                    Login With Facebook
+                                                </a>
+                                            </div><!-- End .col-6 -->
+                                        </div><!-- End .row -->
+                                    </div><!-- End .form-choice -->
+                                </div><!-- .End .tab-pane -->
+                                <div class="tab-pane fade" id="register" role="tabpanel" aria-labelledby="register-tab">
+                                    <form action="#">
+                                        <div class="form-group">
+                                            <label for="register-email">Your email address *</label>
+                                            <input type="email" class="form-control" id="register-email" name="register-email" required>
+                                        </div><!-- End .form-group -->
+
+                                        <div class="form-group">
+                                            <label for="register-password">Password *</label>
+                                            <input type="password" class="form-control" id="register-password" name="register-password" required>
+                                        </div><!-- End .form-group -->
+
+                                        <div class="form-footer">
+                                            <button type="submit" class="btn btn-outline-primary-2">
+                                                <span>SIGN UP</span>
+                                                <i class="icon-long-arrow-right"></i>
+                                            </button>
+
+                                            <div class="custom-control custom-checkbox">
+                                                <input type="checkbox" class="custom-control-input" id="register-policy" required>
+                                                <label class="custom-control-label" for="register-policy">I agree to the <a href="#">privacy policy</a> *</label>
+                                            </div><!-- End .custom-checkbox -->
+                                        </div><!-- End .form-footer -->
+                                    </form>
+                                    <div class="form-choice">
+                                        <p class="text-center">or sign in with</p>
+                                        <div class="row">
+                                            <div class="col-sm-6">
+                                                <a href="#" class="btn btn-login btn-g">
+                                                    <i class="icon-google"></i>
+                                                    Login With Google
+                                                </a>
+                                            </div><!-- End .col-6 -->
+                                            <div class="col-sm-6">
+                                                <a href="#" class="btn btn-login  btn-f">
+                                                    <i class="icon-facebook-f"></i>
+                                                    Login With Facebook
+                                                </a>
+                                            </div><!-- End .col-6 -->
+                                        </div><!-- End .row -->
+                                    </div><!-- End .form-choice -->
+                                </div><!-- .End .tab-pane -->
+                            </div><!-- End .tab-content -->
+                        </div><!-- End .form-tab -->
+                    </div><!-- End .form-box -->
+                </div><!-- End .modal-body -->
+            </div><!-- End .modal-content -->
+        </div><!-- End .modal-dialog -->
+    </div><!-- End .modal -->
+
+
+    <script src= "{{asset('assets/js/jquery.min.js')}}"></script>
+
+    <script src= "{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+
+    <script src= "{{asset('assets/js/jquery.hoverIntent.min.js')}}"></script>
+
+    <script src= "{{asset('assets/js/jquery.waypoints.min.js')}}"></script>
+
+    <script src= "{{asset('assets/js/superfish.min.js')}}"></script>
+
+    <script src= "{{asset('assets/js/owl.carousel.min.js')}}"></script>
+
+    <script src= "{{asset('assets/js/bootstrap-input-spinner.js')}}"></script>
+
+    <script src= "{{asset('assets/js/jquery.magnific-popup.min.js')}}"></script>
+
+    <script src= "{{asset('assets/js/jquery.plugin.min.js')}}"></script>
+
+    <script src= "{{asset('assets/js/jquery.countdown.min.js')}}"></script>
+
+    <!-- Main JS File -->
+    <script src= "{{asset('assets/js/main.js')}}"></script>
+
+    <script src= "{{asset('assets/js/demos/demo-13.js')}}"></script>
+
+</body>
+
+
+<!-- molla/index-13.html  22 Nov 2019 09:59:31 GMT -->
+</html>
